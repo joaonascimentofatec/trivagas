bdb75a2dfdb88ed4f636ccacc664a0bb6727daad
LeonardoDezotiFatec <106208208+LeonardoDezotiFatec@users.noreply.github.com>
2022-06-04 20:30:13 -0300

    Update Modals | Telas

--- cadastro.js
+++ cadastro.js
@@ -1,15 +1,22 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -17,9 +24,18 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+
 }
