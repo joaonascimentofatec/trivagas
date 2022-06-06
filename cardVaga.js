5dc9ed22ff01f23e4cb85cac627b74cd1521e170
joaonascimentofatec <105250640+joaonascimentofatec@users.noreply.github.com>
2022-06-06 20:34:00 -0300

    Card Vagas

---
+++ cardVaga.js
@@ -0,0 +1,9 @@
+document.write(`
+<div style="height: 400px; width: 300px; box-shadow: 2px 2px 5px #555555; border-radius: 25px; float: left; padding: 10px 20px; marging-right: 200px;">
+    <h2 class="texto-azul" style="height: 25%">Estágio em Auxiliar de Programador de Sistemas da Informação</h2>
+
+    <p style="height: 50%">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+    <a href="#" class="botao-azul" id="myBtn" style="float: right; marging-top: 50%; font-size: 1.5em;">Detalhes</a>
+</div>
+`);
